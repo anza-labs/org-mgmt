af40a2dc879f8df27d8a8dfbfb54be6572406428
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-10-06 20:28:48 +0200

    feat: add topics and descriptions to repos
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- repositories.tf
+++ repositories.tf
@@ -1,35 +1,44 @@
 module "org_mgmt" {
-  source   = "./modules/repository"
-  name     = "org-mgmt"
-  archived = false
-  topics   = []
+  source      = "./modules/repository"
+  name        = "org-mgmt"
+  description = "Management of org via OpenTofu and GitHub Actions"
+  archived    = false
+  topics      = ["hacktoberfest", "opentofu"]
 }
 
 module "scribe" {
-  source    = "./modules/repository"
-  name      = "scribe"
-  archived  = false
-  is_public = true
+  source      = "./modules/repository"
+  name        = "scribe"
+  description = "Tool for propagation of annotations in Kubernetes"
+  archived    = false
+  is_public   = true
+  topics      = ["hacktoberfest", "kubernetes"]
 }
 
 module "manifests" {
-  source    = "./modules/repository"
-  name      = "manifests"
-  archived  = false
-  is_public = true
+  source      = "./modules/repository"
+  name        = "manifests"
+  description = "Application manifests for Flux managed clusters"
+  archived    = false
+  is_public   = true
+  topics      = ["hacktoberfest", "kubernetes", "flux", "gitops"]
 }
 
 module "infra" {
-  source    = "./modules/repository"
-  name      = "infra"
-  archived  = false
-  is_public = true
+  source      = "./modules/repository"
+  description = "Infrastructure as a code"
+  name        = "infra"
+  archived    = false
+  is_public   = true
+  topics      = ["hacktoberfest", "kubernetes", "flux", "gitops"]
 }
 
 module "charts" {
   source       = "./modules/repository"
   name         = "charts"
+  description  = "Helm Charts of tools developed or used by Anza Labs"
   archived     = false
   is_public    = true
   enable_pages = true
+  topics       = ["hacktoberfest", "kubernetes", "helm"]
 }
