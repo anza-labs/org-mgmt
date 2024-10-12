580b6f1b3a06c2f6f5a15688fbf974d2c2726326
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-10-12 18:01:53 +0200

    feat: opensource the repo
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- repositories.tf
+++ repositories.tf
@@ -3,7 +3,24 @@ module "org_mgmt" {
   name        = "org-mgmt"
   description = "Management of org via OpenTofu and GitHub Actions"
   archived    = false
+  is_public   = true
   topics      = ["hacktoberfest", "opentofu"]
+
+  labels = [
+    { name = "area/dependency", color = "0052cc", description = "Issues or PRs related to dependency changes." },
+    { name = "do-not-merge", color = "e11d21", description = "Indicates that a PR should not merge." },
+    { name = "kind/membership", color = "c7def8", description = "Categorizes issue or PR as related to a new feature." },
+    { name = "kind/security", color = "e11d21", description = "Categorizes issue or PR as related to a security." },
+    { name = "kind/support", color = "c7def8", description = "Categorizes issue or PR as support-related." },
+  ]
+
+  required_status_checks = [
+    "DCO",
+    "pr-title",
+    "tofu-fmt",
+    "tflint",
+    "shell-linter",
+  ]
 }
 
 module "scribe" {
