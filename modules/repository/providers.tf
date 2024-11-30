f07f55294d7b67866f5aab06013224af67fe19f4
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-11-30 06:50:14 +0100

    fix(linters): general tflint adjustments
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- modules/repository/providers.tf
+++ modules/repository/providers.tf
@@ -1,8 +1,12 @@
+# tflint-ignore: terraform_required_version
 terraform {
   required_providers {
+    # tflint-ignore: terraform_required_providers
     github = {
       source = "integrations/github"
     }
+
+    # tflint-ignore: terraform_required_providers
     bitwarden = {
       source = "maxlaverse/bitwarden"
     }
