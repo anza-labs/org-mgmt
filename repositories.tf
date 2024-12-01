71044f4690c440888700e2b6a50db8dd01c051cc
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-12-01 12:22:30 +0100

    feat: add ssh pub keys
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- repositories.tf
+++ repositories.tf
@@ -1,14 +1,14 @@
 locals {
-  PAT                = "a288b2ae-a336-4425-9b07-b1f100cd05ec"
   GPG_KEYRING_BASE64 = "568fd648-901a-4161-85e4-b1c500b3cb94"
   GPG_PASSPHRASE     = "ffb60bb8-8422-4d3b-95a1-b20700fb5232"
-  PG_CONN_STR        = "b7d22a8b-8185-4d62-8bf6-b1d400b87552"
   LINODE_TOKEN       = "1c579b93-ac50-47a9-85e1-b1c500b40dd5"
   OCI_FINGERPRINT    = "fdc9cf76-3eed-4173-9e66-b1d400b74aaf"
   OCI_PEM_PRV        = "8c29761d-9f79-44f1-97a1-b1d400b70984"
-  OCI_PEM_PUB        = "52c24fe2-2dba-45bb-be55-b1d400d3d7a8"
   OCI_TENANCY_OCID   = "f65d2583-7fe2-4ead-a4fa-b1d400b72191"
   OCI_USER_OCID      = "be76a461-2f01-49b8-a106-b1d400b735f4"
+  PAT                = "a288b2ae-a336-4425-9b07-b1f100cd05ec"
+  PG_CONN_STR        = "b7d22a8b-8185-4d62-8bf6-b1d400b87552"
+  SSH_PUB_KEY        = "b2b149a9-1a9c-4a45-b3c6-b1d400b48405"
 }
 
 module "org_mgmt" {
@@ -85,14 +85,14 @@ module "infra" {
   topics      = ["hacktoberfest", "kubernetes", "flux", "gitops"]
 
   secrets = [
-    { name = "PAT", secret_id = local.PAT },
     { name = "LINODE_TOKEN", secret_id = local.LINODE_TOKEN },
-    { name = "PG_CONN_STR", secret_id = local.PG_CONN_STR },
     { name = "OCI_FINGERPRINT", secret_id = local.OCI_FINGERPRINT },
     { name = "OCI_PEM_PRV", secret_id = local.OCI_PEM_PRV },
-    { name = "OCI_PEM_PUB", secret_id = local.OCI_PEM_PUB },
     { name = "OCI_TENANCY_OCID", secret_id = local.OCI_TENANCY_OCID },
     { name = "OCI_USER_OCID", secret_id = local.OCI_USER_OCID },
+    { name = "PG_CONN_STR", secret_id = local.PG_CONN_STR },
+    { name = "PAT", secret_id = local.PAT },
+    { name = "SSH_PUB_KEY", secret_id = local.SSH_PUB_KEY },
   ]
 }
 
