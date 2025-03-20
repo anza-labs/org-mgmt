99569eec1f7545176a42f8e3a54ee21f8a5770a1
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2025-03-20 12:12:25 +0100

    chore: fmt
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- repositories.tf
+++ repositories.tf
@@ -3,7 +3,7 @@ locals {
   DISCORD_WEBHOOK_FLUX_EU_1 = "693e8bd4-4cb4-4a5b-9ab7-b25601604439"
   GPG_KEYRING_BASE64        = "568fd648-901a-4161-85e4-b1c500b3cb94"
   GPG_PASSPHRASE            = "ffb60bb8-8422-4d3b-95a1-b20700fb5232"
-  COMMIT_SIGNING = "4c5c18b1-5412-4b71-85e1-b29e009d6cbd"
+  COMMIT_SIGNING            = "4c5c18b1-5412-4b71-85e1-b29e009d6cbd"
   LINODE_TOKEN              = "1c579b93-ac50-47a9-85e1-b1c500b40dd5"
   OCI_FINGERPRINT           = "fdc9cf76-3eed-4173-9e66-b1d400b74aaf"
   OCI_PEM_PRV               = "8c29761d-9f79-44f1-97a1-b1d400b70984"
@@ -230,12 +230,12 @@ module "website" {
 }
 
 module "cosi-sample-app" {
-  source        = "./modules/repository"
-  name          = "cosi-sample-app"
-  description   = ""
-  archived      = false
-  is_public     = true
-  topics        = ["hacktoberfest", "container-object-storage-interface", "object-storage"]
+  source      = "./modules/repository"
+  name        = "cosi-sample-app"
+  description = ""
+  archived    = false
+  is_public   = true
+  topics      = ["hacktoberfest", "container-object-storage-interface", "object-storage"]
 
   labels = [
     { name = "area/dependency", color = "0052cc", description = "Issues or PRs related to dependency changes." },
