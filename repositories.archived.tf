2e5d43037bc1549b24c16c2d8861ec20abb1ce81
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2025-05-23 11:13:59 +0200

    chore: archive image-builder
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- repositories.archived.tf
+++ repositories.archived.tf
@@ -19,3 +19,27 @@ module "scribe" {
     { name = "PAT", secret_id = local.PAT },
   ]
 }
+
+module "image-builder" {
+  source       = "./modules/repository"
+  name         = "image-builder"
+  description  = "Tool for building linuxkit images in Kubernetes"
+  archived     = true
+  is_public    = true
+  topics       = ["hacktoberfest", "kubernetes", "linuxkit"]
+  enable_pages = true
+  homepage_url = "anza-labs.github.io/image-builder"
+
+  required_status_checks = [
+    "DCO",
+    "pr-title",
+    "unit",
+    # "e2e", e2e are not working for image builder in CI due to limited memory, fix needed
+    "lint",
+    "hadolint",
+  ]
+
+  secrets = [
+    { name = "PAT", secret_id = local.PAT },
+  ]
+}
--- repositories.tf
+++ repositories.tf
@@ -153,30 +153,6 @@ module "charts" {
   ]
 }
 
-module "image-builder" {
-  source       = "./modules/repository"
-  name         = "image-builder"
-  description  = "Tool for building linuxkit images in Kubernetes"
-  archived     = false
-  is_public    = true
-  topics       = ["hacktoberfest", "kubernetes", "linuxkit"]
-  enable_pages = true
-  homepage_url = "anza-labs.github.io/image-builder"
-
-  required_status_checks = [
-    "DCO",
-    "pr-title",
-    "unit",
-    # "e2e", e2e are not working for image builder in CI due to limited memory, fix needed
-    "lint",
-    "hadolint",
-  ]
-
-  secrets = [
-    { name = "PAT", secret_id = local.PAT },
-  ]
-}
-
 module "kink" {
   source       = "./modules/repository"
   name         = "kink"
